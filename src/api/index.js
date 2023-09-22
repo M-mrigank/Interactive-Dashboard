e799f35f98b7aa42874ae2434df7863095b3c0c6
M-mrigank <mrigank.mgr@example.com>
2023-09-22 15:42:56 +0530

    upp1

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// const API=axios.create({baseURL:'http://localhost:5000'});
 const API=axios.create({baseURL:'https://dashboard-backend-j0ya.onrender.com'});
+
 
 export const getAllData=()=>API.get(`/data/getAllData`);
